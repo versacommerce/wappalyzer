ea4610969d4ea865209556e190279f5c80bd580a
Björn B. Dorra <dorra@d-1.com>
2022-04-07 16:02:20 +0000

    setup, docker file with more installers

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "==> INSTALLING ADDITIONAL PACKAGES"
+echo "--> BREW TAP RECEIPES"
+# brew tap stripe/stripe-cli
+# brew tap elastic/tap
+echo "--> BREW INSTALL PACKAGES"
+# brew install stripe/stripe-cli/stripe
+# brew install elastic/tap/elasticsearch-full
+echo "--> GEM INSTALL PACKAGES"
+# gem install bundler
+# gem install foreman
+echo "--> INSTALL HEROKU CLI"
+curl https://cli-assets.heroku.com/install.sh | sh
